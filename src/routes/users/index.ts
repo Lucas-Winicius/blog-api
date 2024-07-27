c9ff0ed2ea26c18da846ca3de0c63b61f71b1a48
Lucas-Winicius <lucasclucas7@gmail.com>
2024-07-27 17:23:08 -0300

    route to get user info

--- src/routes/users/index.ts
+++ src/routes/users/index.ts
@@ -1,10 +1,12 @@
 import { app } from '../../server'
 import get from './get'
+import info from './info'
 import patch from './patch'
 import post from './post'
 
 export default async function UserRoutes() {
   app.register(post)
   app.register(patch)
+  app.register(info)
   app.register(get)
 }
---
+++ src/routes/users/info.ts
@@ -0,0 +1,46 @@
+import { FastifyReply, FastifyInstance, FastifyRequest } from 'fastify'
+import { user } from '../../db/schema/users'
+import { db } from '../../db/db'
+import userCredentials from '../../auth/userCredentials'
+import { eq } from 'drizzle-orm'
+import redis from '../../db/redis'
+
+export default async function (app: FastifyInstance) {
+  app.route({
+    method: 'GET',
+    url: '/users/info',
+    preHandler: app.auth([userCredentials]),
+    handler: async (request: FastifyRequest, reply: FastifyReply) => {
+      const cachedUserInfo = await redis.get(`userinfo:${request.user}`)
+
+      if (cachedUserInfo)
+        return reply.status(200).send(JSON.parse(cachedUserInfo))
+
+      const userData = await db.query.user.findMany({
+        columns: {
+          name: true,
+          username: true,
+          role: true,
+          createdAt: true,
+          updatedAt: true,
+        },
+        where: eq(user.id, parseInt(request.user.toString())),
+        with: {
+          posts: {
+            columns: {
+              id: true,
+              title: true,
+              slug: true,
+              createdAt: true,
+              updatedAt: true,
+            },
+          },
+        },
+      })
+
+      await redis.set(`userinfo:${request.user}`, JSON.stringify(userData[0]))
+
+      return reply.status(200).send(userData[0])
+    },
+  })
+}
