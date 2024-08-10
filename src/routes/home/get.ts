2f1ada701948427a0c42326795d12a76891da3ab
Lucas-Winicius <lucasclucas7@gmail.com>
2024-08-09 23:42:36 -0300

    the root now returns the collaborators

--- src/routes/home/get.ts
+++ src/routes/home/get.ts
@@ -2,6 +2,8 @@ import { FastifyReply, FastifyInstance, FastifyRequest } from 'fastify'
 import { db } from '../../db/db'
 import { post } from '../../db/schema/posts'
 import redis from '../../db/redis'
+import { user } from '../../db/schema/users'
+import { eq, or } from 'drizzle-orm'
 
 export default async function (app: FastifyInstance) {
   app.get('/', async function (request: FastifyRequest, reply: FastifyReply) {
@@ -10,22 +12,34 @@ export default async function (app: FastifyInstance) {
     if (cache) return reply.status(200).send(JSON.parse(cache))
 
     const postData = await db.select().from(post)
+    const contributorsData = await db
+      .select()
+      .from(user)
+      .where(or(eq(user.role, 'contributor'), eq(user.role, 'admin')))
 
-    if (postData.length) {
-      postData.forEach((post, id) => {
-        redis.set(`post:id:${post.id}`, JSON.stringify(postData[id]))
-        redis.set(`post:slug:${post.slug}`, JSON.stringify(postData[id]))
-      })
+    let recomended = new Array(5)
+    let contributors = new Array(10)
+
+    if (postData.length < 5) {
+      recomended = [...postData]
+    } else {
+      for (let i = 0; i < 5; i++) {
+        const random = Math.floor(Math.random() * postData.length - 1)
+        recomended[i] = postData[random]
+      }
     }
 
-    const recomended = new Array(10)
-
-    for (let i = 0; i < 10; i++) {
-      const random = Math.floor(Math.random() * postData.length - 1)
-      recomended[i] = postData[random]
+    if (contributorsData.length < 10) {
+      contributors = [...contributorsData]
+    } else {
+      for (let i = 0; i < 10; i++) {
+        const random = Math.floor(Math.random() * contributorsData.length - 1)
+        contributors[i] = contributorsData[random]
+      }
     }
 
-    const homePage = { recomended, posts: postData }
+
+    const homePage = { recomended, contributors, posts: postData }
 
     redis.set(`post:system:home`, JSON.stringify(homePage))
     return reply.status(200).send(homePage)
