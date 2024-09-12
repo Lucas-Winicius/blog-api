d8be7925973d9a0f6669d58fb90ffc412482aef8
Lucas-Winicius <lucasclucas7@gmail.com>
2024-09-12 18:16:01 -0300

    fix if user created

--- src/routes/users/post.ts
+++ src/routes/users/post.ts
@@ -1,6 +1,7 @@
 import { FastifyReply, FastifyInstance, FastifyRequest } from 'fastify'
 import { insertUserSchema, user } from '../../db/schema/users'
 import { db } from '../../db/db'
+import { eq } from 'drizzle-orm'
 
 type UserBody = {
   name: string
@@ -18,12 +19,20 @@ export default async function (app: FastifyInstance) {
     ) => {
       const userData = await insertUserSchema.safeParseAsync(request.body)
 
+      const userOnDb = await db.query.user.findFirst({
+        where: eq(user.username, userData.data?.username),
+      })
+
+      if (userOnDb)
+        return reply
+          .status(400)
+          .send({ status: 400, message: 'This user already exists' })
+
       if (userData.success) {
         await db
           .insert(user)
           .values(userData.data as unknown as UserBody)
           .returning()
-          .onConflictDoNothing()
 
         return reply.status(201).send({
           status: 201,
